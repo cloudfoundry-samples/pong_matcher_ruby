bf3cfb19423418168733b5244b717c9087aa583c
Andrew Bruce <andrew@camelpunch.com>
2014-10-13 17:56:19 +0100

    Remove duplicate use of -n in cf push

--- deploy.sh
+++ deploy.sh
@@ -7,4 +7,4 @@ docker run -t docker.gocd.cf-app.com:5000/pong-matcher-ruby /bin/bash -c "\
     cf api https://api.run.pivotal.io
     cf auth $CF_USERNAME $CF_PASSWORD &&
     cf target -o $CF_ORG -s $CF_SPACE &&
-    cf push -n $HOSTNAME -d cfapps.io -n rubypong"
+    cf push -n $HOSTNAME -d cfapps.io"
