aaa54b435cc283b7a1e143dd56f1c24cac942d89
Andrew Bruce <andrew@camelpunch.com>
2014-10-16 13:15:18 +0000

    Attempt to pull image before building
    Guard unsuccessful pull

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
 
+set -ex
+
+docker pull docker.gocd.cf-app.com:5000/pong-matcher-ruby || true
 docker build -t docker.gocd.cf-app.com:5000/pong-matcher-ruby .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-ruby
