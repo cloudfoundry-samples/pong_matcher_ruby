316204d959ad3df5672dd8c9e6567d9f60e4ef4f
Andrew Bruce <andrew@camelpunch.com>
2014-10-24 14:45:28 +0100

    DOCKER_BUILD_OPTS functionality
    Allows e.g. to --no-cache=true when packages change.

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 set -ex
 
 docker pull docker.gocd.cf-app.com:5000/pong-matcher-ruby || true
-docker build -t docker.gocd.cf-app.com:5000/pong-matcher-ruby .
+docker build $DOCKER_BUILD_OPTS -t docker.gocd.cf-app.com:5000/pong-matcher-ruby .
 docker push docker.gocd.cf-app.com:5000/pong-matcher-ruby
